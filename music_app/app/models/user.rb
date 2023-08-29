e43568e429f2ec40bbea3fb819e082340064d3ee
David <2018dpollack@gmail.com>
2023-08-29 11:04:12 -0700

    Added user model

---
+++ music_app/app/models/user.rb
@@ -0,0 +1,56 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    before_validation :ensure_session_token
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true, 
+    validates :password, allow_nil: true, length: {minimum: 6}
+    attr_reader :password
+end
+
+def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    if user && user.is_password?(password)
+        user
+    else
+        nil
+    end
+end
+
+def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+end
+
+def is_password?(password)
+    password_obj = BCrypt::Password.new(self.password_digest)
+    password_obj.is_password?(password)
+end
+
+def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+end
+
+def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+end
+
+def generate_unique_session_token
+    while true
+        session_token = SecureRandom.urlsafe_base64
+        return session_token unless User.exists?(session_token: session_token)
+    end
+end
+
